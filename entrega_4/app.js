6a2de5f9a7d7d14da17431bb14361be9bc9567e9
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-01-14 19:46:07 -0300

    Add server

---
+++ entrega_4/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import routerProducts from "./routes/router.products.js";
+import routerCart from "./routes/router.cart.js";
+
+//CONFIGURACIONES
+const app = express();
+const PORT = 8080;
+
+//MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//RUTAS
+app.use("/api/products", routerProducts);
+app.use("/api/cart", routerCart);
+
+//INICIAR SERVIDOR
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${PORT}`);
+});
+
+//MANEJO DE ERRORES
+server.on("error", (error) => {
+    console.log(`Error en servidor ${error}`);
+});
