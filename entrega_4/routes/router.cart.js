9d827a27c15084b190443aac1ee6a8c2b0c1a233
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-01-15 19:35:04 -0300

    Add diferentes endpoints

--- entrega_4/routes/router.cart.js
+++ entrega_4/routes/router.cart.js
@@ -1,9 +1,40 @@
 import { Router } from "express";
+import CartManager from "../model/CartManager.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Estoy en el router de Cart");
+router.get("/:cid", async (req, res) => {
+  try {
+    const cid = parseInt(req.params.cid);
+    const cartManager = new CartManager();
+    const cart = await cartManager.getCartById(cid);
+    res.status(200).json({ status: "success", payload: cart });
+  } catch (error) {
+    res.status(400).json({ status: "error", error: "Ocurrió un error" });
+  }
+});
+
+router.post("/:cid/product/:pid", async (req, res) => {
+  try {
+    const cid = parseInt(req.params.cid);
+    const pid = parseInt(req.params.pid);
+    const { quantity } = req.body;
+    const cartManager = new CartManager();
+    const cart = await cartManager.updateCart(cid, pid, quantity);
+    res.status(200).json({ status: "success", payload: cart });
+  } catch (error) {
+    res.status(400).json({ status: "error", error: "Ocurrió un error" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const cartManager = new CartManager();
+    const cart = await cartManager.addCart();
+    res.status(201).json({ status: "success", cart });
+  } catch (error) {
+    res.status(500).json({ status: "error", error: error.message });
+  }
 });
 
 export default router;
