93f95376aefac3cb95c7a55ebbbd59ef15b48001
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-17 19:12:42 -0300

    Init base de datos

---
+++ pre_entrega_final_2/src/dbConfig.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+//VARIABLE DE ENTORNO
+const URI = 'mongodb+srv://admin:CGfcsW93xVPFluzk@cluster0.62nsj.mongodb.net/ecommerce';
+
+// CONEXION A LA BASE DE DATOS
+const dbConfig = async () => {
+    try {
+        await mongoose.connect(URI);
+        console.log('La base de datos se ha conectado');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default dbConfig;
