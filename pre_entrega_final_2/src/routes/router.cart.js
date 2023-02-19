8967bd19bc8babc1737fdb711eed00f67c5b2dfc
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-19 20:25:41 -0300

    fix asincronismo

--- pre_entrega_final_2/src/routes/router.cart.js
+++ pre_entrega_final_2/src/routes/router.cart.js
@@ -21,19 +21,45 @@ router.get("/:cid", async (req, res) => {
   }
 });
 
-// CREAR UN PRODUCTO EN UN CARRITO
+// AGREGAR UN ARREGLO DE PRODUCTOS AL CARRITO
+router.put("/:cid", async (req, res) => {
+  try {
+    const cid = req.params.cid;
+    const products = req.body;
+    const cartManager = new CartManager();
+    const cart = await cartManager.insertProductsInCart(cid, products);
+    res.status(200).json({ status: "success", payload: cart });
+  } catch (error) {
+    res.status(400).json({ status: "error", error: error.message });
+  }
+});
+
+// VACIAR CARRITO
+router.delete("/:cid/product", async (req, res) => {
+  try {
+    const cid = req.params.cid;
+    const cartManager = new CartManager();
+    const cart = await cartManager.emptyCart(cid);
+    res.status(200).json({ status: "success", payload: cart });
+  } catch (error) {
+    res.status(400).json({ status: "error", error: error.message });
+  }
+});
+
+// ACTUALIZAR UN PRODUCTO EN UN CARRITO
 router.post("/:cid/product/:pid", async (req, res) => {
   try {
     const cid = req.params.cid;
     const pid = req.params.pid;
     const { quantity } = req.body;
     const cartManager = new CartManager();
-    const cart = await cartManager.updateCart(cid, pid, quantity);
-    res.status(200).json({ status: "success", payload: cart });
+    await cartManager.updateCart(cid, pid, quantity);
+    res.redirect(`/carts/${cid}`);
   } catch (error) {
     res.status(400).json({ status: "error", error: "Ocurrió un error" });
   }
 });
+
 
 // CREAR UN CARRITO
 router.post("/", async (req, res) => {
@@ -47,29 +73,28 @@ router.post("/", async (req, res) => {
 });
 
 // DELETE PRODUCTO DE UN CARRITO
-router.delete('/:cid/product/:pid', async (req, res)=>{
+router.delete("/:cid/product/:pid", async (req, res) => {
   try {
     const cartManager = new CartManager();
     const cid = req.params.cid;
     const pid = req.params.pid;
     const cart = await cartManager.deleteProduct(cid, pid);
-    res.status(200).json({status: "success", payload: cart});
+    res.status(200).json({ status: "success", payload: cart });
   } catch (error) {
-      res.status(500).json({ status: "error", error: error.message });
+    res.status(500).json({ status: "error", error: error.message });
   }
 });
 
 // DELETE CARRITO
-router.delete('/:cid/product/:pid', async (req, res)=>{
+router.delete("/:cid/", async (req, res) => {
   try {
     const cartManager = new CartManager();
     const cid = req.params.cid;
-   const cart = cartManager.deleteCart(cid);
-    res.status(200).json({status: "success", payload: cart});
+    const cart = await cartManager.deleteCart(cid);
+    res.status(200).json({ status: "success", payload: cart });
   } catch (error) {
     res.status(500).json({ status: "error", error: error.message });
   }
-  
 });
 
 export default router;
