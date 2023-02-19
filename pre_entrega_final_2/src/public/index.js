0f730294f8b40a5e6a3127e894b654f6a9142ee2
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-19 20:25:00 -0300

    Add Fetch para actualizar cantidad de carrito

--- pre_entrega_final_2/src/public/index.js
+++ pre_entrega_final_2/src/public/index.js
@@ -3,10 +3,6 @@ const socket = io();
 
 // **** VARIABLES DOM **** //
 const cards = document.getElementById("cards");
-const nombreUsuario = document.getElementById("nombreUsuario");
-const chatParrafo = document.getElementById("chatParrafo");
-const chatContainer = document.getElementById("chatContainer");
-
 
 // **** PRODUCTOS **** //
 
@@ -38,65 +34,34 @@ socket.on("productos", (prods) => {
   });
 });
 
+// **** FETCHS **** //
 
-// **** CHAT **** //
+//Actualizar cantidad en producto al carrito
+const productCart = document.querySelectorAll(".cartDescription")
+productCart.forEach(cart => {
+  cart.children[3].addEventListener("click", async (e) => {
+    e.preventDefault();
 
-let usuario = null;
+    const pid = e.target.getAttribute("id-product");
+    const cid = e.target.getAttribute("id-cart");
 
-// Alerta para ingresar nombre de usuario
-if (!usuario) {
-  // eslint-disable-next-line no-undef
-  Swal.fire({
-    title: "BIENVENIDO",
-    text: "Ingrese su nombre de usuario",
-    input: "text",
-    inputValidator: (value) => {
-      if (!value) return "Debe ingresar un nombre de usuario";
-    },
-  })
-    .then((result) => {
-      // Obtener nombre de usuario
-      usuario = result.value ? result.value : "Anónimo";
-      nombreUsuario.innerHTML = usuario;
+    postCart(cid, pid, cart.children[2].value)
+   
+  });
+})
 
-      // Agregar usuario a input hidden
-      document.getElementById("usuario").value = usuario;
-
-      // Emitir nombre de usuario para bradcast
-      socket.emit("newUser", usuario);
-    })
-    .catch((error) => {
-      console.log(error);
+// Actualizar carrito
+async function postCart(cid, pid, quantity){
+  try {
+    await fetch(`/api/carts/${cid}/product/${pid}`, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({quantity}),
     });
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-socket.on("messages", (messages) => {
-
-  // Renderizar mensajes
-  chatParrafo.innerHTML = messages.map(msg => {
-    return `
-    <p class="msg"><span class="horaChat">${msg.fecha.split('T')[0] + ' - ' + msg.fecha.split('T')[1].split('.')[0]}</span> <span><strong>${msg.usuario}:</strong> ${msg.mensaje}</span></p>
-    `;
-  }).join('');
-
-  // Clean input
-  document.getElementById("mensaje").value = "";
-
-  // Scroll automático al final del chat
-  chatContainer.scrollTop = chatContainer.scrollHeight - chatContainer.clientHeight;
-})
-
-// Toast para nuevo usuario
-socket.on('broadcast', (newUser) => {
-  // eslint-disable-next-line no-undef
-  Swal.fire({
-    position: 'top-right',
-    toast: true,
-    timer: 3000,
-    showConfirmButton: false,
-    icon: 'success',
-    iconColor: 'white',
-    timerProgressBar: true,
-    text: `${newUser} se ha unido al chat`,
-  })
-})
