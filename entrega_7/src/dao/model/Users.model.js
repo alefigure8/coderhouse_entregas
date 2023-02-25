cf5d7ead9d1938fa13497c6f7a1ac53e10df0f8a
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-25 16:56:32 -0300

    Init base de datos

---
+++ entrega_7/src/dao/model/Users.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    require: true,
+  },
+  lastname: {
+    type: String,
+    require: true,
+  },
+  age: {
+    type: Number,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+export const usersModels = mongoose.model("Users", userSchema);
