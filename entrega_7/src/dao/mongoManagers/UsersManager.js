f5c8910e5a5df74005e375a1c17a35a77a2045e4
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-26 19:47:45 -0300

    Init user manager

---
+++ entrega_7/src/dao/mongoManagers/UsersManager.js
@@ -0,0 +1,58 @@
+import { usersModels } from "../model/Users.model.js";
+
+/*
+CarManager
+  -getCarts
+  -getCartById
+  -addCarts
+  -updateCart
+  -deleteCart
+  -deleteProduct
+  -emptyCart
+*/
+
+
+class UsersManager {
+  constructor() {
+    this.users = [];
+  }
+
+  async getUserByEmailAndPassword(email, password) {
+    try {
+      const user = await usersModels.findOne({email, password});
+      return user;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+   // Obtener usuario por mail
+  async getUserByEmail(email) {
+    try {
+      const user = await usersModels.findOne({email}).lean();
+      return user;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  // Crear usuario
+  async addUser(user) {
+    try {
+      const newUser = usersModels.create(user);
+      return newUser;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  
+
+
+
+
+
+
+}
+
+export default UsersManager;
