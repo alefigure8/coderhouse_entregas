f5562d48477e2d2fc04ffa6da90ecd25314b03fb
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-03-11 08:33:20 -0300

    Add bcrypt

---
+++ entrega_8/src/utils.js
@@ -0,0 +1,24 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bcrypt from 'bcrypt'
+
+// **** DIRNAME **** //
+export const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// **** BCRYPT **** //
+
+//Hash password
+const SALTROUNDS = 10;
+
+const generateSalt = async () => {
+    return bcrypt.genSalt(SALTROUNDS);
+}
+export const hashPass = async pass => {
+    const salt = await generateSalt();
+    return bcrypt.hash(pass, salt);
+}
+
+//Compare password
+export const comparePassword = async (userPass, hashPass) => {
+    return bcrypt.compare(userPass, hashPass)
+}
