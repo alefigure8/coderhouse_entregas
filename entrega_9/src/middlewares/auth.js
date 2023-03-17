705e80b3bb66226e9da6437adf47981aa61f04f0
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-03-17 15:57:26 -0300

    create middleware para jwt

--- entrega_9/src/middlewares/auth.js
+++ entrega_9/src/middlewares/auth.js
@@ -1,6 +1,8 @@
 //import UsersManager from "../dao/mongoManagers/UsersManager.js";
+import jwt from "jsonwebtoken";
 
-export const auth = async(req, res, next) => {
+// SESSION
+export const auth = (req, res, next) => {
   if (req?.session?.logged) {
     res.user = req.session;
     next();
@@ -9,3 +11,14 @@ export const auth = async(req, res, next) => {
     next();
   }
 };
+
+// JWT
+export const jwtAuth = (req, res, next) => {
+  const isValid = jwt.verify(req.cookies["token"], process.env.JWT_SECRET);
+  if (isValid) {
+    res.user = isValid;
+    next();
+  } else {
+    res.status(401).json({ error: "No autorizado" });
+  }
+}
