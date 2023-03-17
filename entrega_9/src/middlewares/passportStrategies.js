ccdd297f96e5fbaa265c976e9171c59cc700e1fc
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-03-17 15:57:46 -0300

    create estrategia de jwt

--- entrega_9/src/middlewares/passportStrategies.js
+++ entrega_9/src/middlewares/passportStrategies.js
@@ -1,7 +1,11 @@
 import passport from "passport";
 import { Strategy as githubStrategy } from "passport-github2";
+import { ExtractJwt, Strategy as jwtStrategy } from "passport-jwt";
 import UsersManager from "../dao/mongoManagers/UsersManager.js";
 import dotenv from "dotenv";
+import {cookieExtractor} from "../utils.js";
+
+// Config
 dotenv.config();
 
 // **** GITHUB **** //
@@ -45,3 +49,20 @@ passport.deserializeUser(async (_id, done) => {
   const usuario = await userManager.getUserById(_id);
   done(null, usuario);
 });
+
+
+// **** JWT **** //
+
+passport.use(
+  "jwt",
+  new jwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
+      // eslint-disable-next-line no-undef
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    async (payload, done) => {
+      done(null, payload);
+    }
+  )
+);
