b137b4d4f37519e3c255bc4c10a59d626281be90
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-03-18 17:58:09 -0300

    Delete session middleware

--- entrega_9/src/middlewares/auth.js
+++ entrega_9/src/middlewares/auth.js
@@ -1,20 +1,11 @@
 //import UsersManager from "../dao/mongoManagers/UsersManager.js";
 import jwt from "jsonwebtoken";
 
-// SESSION
-export const auth = (req, res, next) => {
-  if (req?.session?.logged) {
-    res.user = req.session;
-  } else {
-    res.user = undefined;
-  }
-  next();
-};
-
 // JWT
 export const jwtAuth = (req, res, next) => {
+  const token = req.signedCookies.token;
   // eslint-disable-next-line no-undef
-  jwt.verify(req.cookies.token, process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (decoded) {
       res.user = decoded;
     } else {
