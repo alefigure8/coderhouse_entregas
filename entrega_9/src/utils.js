1404b478a315b1f8c3caffa416830cacc8b5d592
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-03-18 17:57:55 -0300

    cambiando comentarios

--- entrega_9/src/utils.js
+++ entrega_9/src/utils.js
@@ -1,41 +1,44 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // **** DIRNAME **** //
 export const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // **** BCRYPT **** //
 
-//Hash password
 const SALTROUNDS = 10;
 
+//Generate salt
 const generateSalt = async () => {
-    return bcrypt.genSalt(SALTROUNDS);
-}
+  return bcrypt.genSalt(SALTROUNDS);
+};
 
-export const hashPass = async pass => {
-    const salt = await generateSalt();
-    return bcrypt.hash(pass, salt);
-}
+//Hash password
+export const hashPass = async (pass) => {
+  const salt = await generateSalt();
+  return bcrypt.hash(pass, salt);
+};
 
 //Compare password
 export const comparePassword = async (userPass, hashPass) => {
-    return bcrypt.compare(userPass, hashPass)
-}
+  return bcrypt.compare(userPass, hashPass);
+};
 
 // **** JWT **** //
 
 //Generate token
 export const generateToken = async (user) => {
-    // eslint-disable-next-line no-undef
-    const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '1h'});
-    return token;
-}
+  // eslint-disable-next-line no-undef
+  const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+  return token;
+};
 
+// **** COOKIE **** //
 
-export const cookieExtractor = (req)=>{
-	const token = req?.cookies?.token;
-	return token;
-}
+//Cookie extractor
+export const cookieExtractor = (req) => {
+  const token = req?.signedCookies?.token;
+  return token;
+};
