1f0f8c8cd4338516479d678af8ded791d7377741
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-03-24 16:03:09 -0300

    dotenv config

---
+++ rebuild_1/src/utils/config.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import {options} from './commander.js';
+
+dotenv.config({
+    path: options.mode === 'stage' ? '.env.stage' : '.env.development'
+});
+
+export const config = {
+    port: process.env.PORT,
+    mongoUrl: process.env.MONGO_URI,
+    mongoPass: process.env.MONGO_PASS,
+    jwtSecret: process.env.JWT_SECRET,
+    cookieSecret: process.env.COOKIE_SECRET,
+    githubClientId: process.env.GITHUB_CLIENT_ID,
+    githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+};
