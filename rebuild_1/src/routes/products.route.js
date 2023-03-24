2129a69463352b0bd6c3aacdb916ae51f31a1a08
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-03-24 16:02:45 -0300

    Create Product Route

---
+++ rebuild_1/src/routes/products.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+import {getProducts, getProduct, postProduct, putProduct, deleteProductById} from '../controllers/products.controller.js';
+const router = Router();
+
+router.get('/', getProducts);
+router.get('/:id', getProduct);
+router.post('/', postProduct);
+router.put('/:id', putProduct);
+router.delete('/:id', deleteProductById);
+
+export default router;
