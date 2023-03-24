d884cf0e9e25bd06c424e6e1cd12556d71535040
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-03-24 16:03:40 -0300

    app main

---
+++ rebuild_1/src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+//import './utils/commander.js'
+import {__dirname} from "././utils/path.js";
+import {config} from "./utils/config.js";
+import './dbConfig.js';
+
+import productsRoute from "./routes/products.route.js";
+
+//CONFIGURACIONES
+const app = express();
+
+//PUBLIC
+app.use(express.static(__dirname + "/public"));
+
+//MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//ROUTES
+app.use("/api/products", productsRoute);
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+//INICIAR SERVIDOR
+const serverHTTP = app.listen(config.port, () => {
+    console.log(`Server listen on port ${config.port}`);
+  });
+
+//MANEJO DE ERRORES
+serverHTTP.on("error", (error) => {
+    console.log(`Server error: ${error}`);
+});
