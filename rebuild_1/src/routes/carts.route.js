319c19fca20fccc34d56e389b1b096467b0c3bfc
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-03-26 16:40:05 -0300

    Add cart route

---
+++ rebuild_1/src/routes/carts.route.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+
+import {
+  deleteCart,
+  deleteProductInCart,
+  deleteProductsInCart,
+  getCart,
+  postCart,
+  putCart,
+  putProductInCart,
+} from "../controllers/carts.controller.js";
+
+const router = Router();
+
+// Ver carrito
+router.get("/:cid", getCart);
+
+// Crear un carrito
+router.post("/", postCart);
+
+// Agregar un producto o arreglo de productos al carrito
+router.put("/:cid", putCart);
+
+// Eliminar carrito
+router.delete("/:cid/", deleteCart);
+
+// Actualizar un producto del carrito
+router.put("/:cid/product/:pid", putProductInCart);
+
+// Eliminar un producto del carrito
+router.delete("/:cid/product/:pid",deleteProductInCart);
+
+// Vaciar el carrito
+router.delete("/:cid/product", deleteProductsInCart);
+
+export default router;
