1946fe7686a4fd3cd4840541ec936ed6d9fd3f52
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-03-26 16:39:35 -0300

    fix metodos cart

--- rebuild_1/src/DAL/mongoManagers/CartManager.js
+++ rebuild_1/src/DAL/mongoManagers/CartManager.js
@@ -1,17 +1,5 @@
 import { cartsModel } from "../model/carts.model.js";
 
-/*
-CarManager
-  -getCarts
-  -getCartById
-  -addCarts
-  -updateCart
-  -deleteCart
-  -deleteProduct
-  -emptyCart
-*/
-
-
 class CartManager {
 
   // Obtener todos los carritos (Solo para desarrollo. Borrar!)
@@ -27,7 +15,7 @@ class CartManager {
   // Obtener el carrito y sus productos
   async getOneCart(option) {
     try {
-      const cart = await cartsModel.find(option).lean();
+      const cart = await cartsModel.findOne(option).lean();
       return cart;
     } catch (error) {
       throw new Error(error);
@@ -63,88 +51,7 @@ class CartManager {
     } catch (error) {
       throw new Error(error);
     }
-  }
-    // async getCartById(id) {
-  //   try {
-  //     const cart = await cartsModel.find({ _id: id }).lean();
-  //     return cart;
-  //   } catch (error) {
-  //     throw new Error(error);
-  //   }
-  // }
-
-
-  // // Crar o Actualizar la cantidad de un producto en el carrito
-  // async updateCart(id, product, quantity) {
-  //   try {
-  //     const cart = await cartsModel.findById(id);
-  //     const productCart = cart.products.find((x) => x.product == product);
-  //     if (productCart) {
-  //       productCart.quantity = quantity;
-  //     } else {
-  //       cart.products.push({product, quantity});
-  //     }
-  //     const updatedCart = await cartsModel.findByIdAndUpdate(id, cart, {new: true});
-  //     return updatedCart;
-  //   } catch (error) {
-  //     throw new Error(error);
-  //   }
-  // }
-
-   // Crar o Actualizar la cantidad de un producto en el carrito
-
-  // //Actualizar la cantidad de un producto
-  // async updateProduct(id, product, quantity) {
-  //   try {
-  //     const cart = await cartsModel.findById(id);
-  //     const productCart = cart.products.find((x) => x.product == product);
-  //     if (productCart) {
-  //       productCart.quantity = quantity;
-  //     }
-  //     const updatedCart = await cartsModel.findByIdAndUpdate(id, cart, {new: true});
-  //     return updatedCart;
-  //   } catch (error) {
-  //     throw new Error(error);
-  //   }
-  // }
-
-  //Agregar arreglo de productos al carrito
-  // async insertProductsInCart(id, products)
-  // {
-  //   try {
-  //     const cart = await cartsModel.findById(id);
-  //     cart.products = [... cart.products, ...products];
-  //     const updatedCart = await cartsModel.findByIdAndUpdate(id, cart);
-  //     return updatedCart;
-  //   } catch (error) {
-  //     throw new Error(error);
-  //   }
-  // }
-
-
-  //Eliminar un producto del carrito
-  // async deleteProduct(id) {
-  //   try {
-  //     const cart = await cartsModel.findById(id);
-  //     cart.products = cart.products.filter((x) => x.id != id);
-  //     await cartsModel.findByIdAndUpdate(id, cart);
-  //     return cart;
-  //   } catch (error) {
-  //     throw new Error(error);
-  //   }
-  // }
-
-  // Vaciar el carrito
-//   async emptyCart(id) {
-//     try {
-//       const cart = await cartsModel.findById(id);
-//       cart.products = [];
-//       await cartsModel.findByIdAndUpdate(id, cart);
-//       return cart;
-//     } catch (error) {
-//       throw new Error(error);
-//     }
-//   }
+  } 
 }
 
 export default CartManager;
