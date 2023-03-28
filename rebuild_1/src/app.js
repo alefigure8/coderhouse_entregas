d32ac09541e203571f59c5325c55d483a8c9f13f
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-03-28 19:27:45 -0300

    Add cookie parser

--- rebuild_1/src/app.js
+++ rebuild_1/src/app.js
@@ -1,14 +1,17 @@
 import express from "express";
-import {__dirname} from "././utils/path.js";
-import {config} from "./utils/config.js";
-import './dbConfig.js';
+import { __dirname } from "././utils/path.js";
+import { config } from "./utils/config.js";
+import cookieParser from "cookie-parser";
+
+import "./dbConfig.js";
 
 import handlebars from "express-handlebars";
-import {helpers} from "./helpers/handlebars.js"
+import { helpers } from "./helpers/handlebars.js";
 
 import productsRoute from "./routes/products.route.js";
 import usersRoute from "./routes/users.route.js";
-import cartsRoute from "./routes/carts.route.js"
+import cartsRoute from "./routes/carts.route.js";
+import viewsRoute from "./routes/views.route.js";
 
 //CONFIGURACIONES
 const app = express();
@@ -17,28 +20,35 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 
 //HANDLEBARS
-app.engine("handlebars", handlebars.engine({
-    helpers
-}));
+app.engine(
+  "handlebars",
+  handlebars.engine({
+    helpers,
+  })
+);
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
-
+console.log(__dirname + "/views");
 
 //MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(config.cookieSecret));
 
-//ROUTES
+//ROUTES API
 app.use("/api/products", productsRoute);
 app.use("/api/users", usersRoute);
-app.use('/api/carts', cartsRoute);
+app.use("/api/carts", cartsRoute);
+
+// ROUTES VIEWS
+app.use("/", viewsRoute);
 
 //INICIAR SERVIDOR
 const serverHTTP = app.listen(config.port, () => {
-    console.log(`Server listen on port ${config.port}`);
-  });
+  console.log(`Server listen on port ${config.port}`);
+});
 
 //MANEJO DE ERRORES
 serverHTTP.on("error", (error) => {
-    console.log(`Server error: ${error}`);
+  console.log(`Server error: ${error}`);
 });
