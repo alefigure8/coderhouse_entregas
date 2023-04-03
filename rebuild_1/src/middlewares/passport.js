22fff485cdd5ffeb02c376e4ab68a704fcf0a3f5
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-04-03 18:02:36 -0300

    create new strategy

--- rebuild_1/src/middlewares/passport.js
+++ rebuild_1/src/middlewares/passport.js
@@ -1,5 +1,16 @@
-import passport from "passport"
+import passport from "passport";
 
-export const githubAuthenticate= passport.authenticate("github", { scope: ["user:email"], session:false })
+export const githubAuthenticate = passport.authenticate("github", {
+  scope: ["user:email"],
+  session: false,
+});
 
-export const githubAuthenticateFailure= passport.authenticate("github", { failureRedirect: "/register", session:false })
+export const githubAuthenticateFailure = passport.authenticate("github", {
+  failureRedirect: "/register",
+  session: false,
+});
+
+export const jwtAuthenticate = passport.authenticate("current", {
+  session: false,
+  failureRedirect: "/products",
+});
