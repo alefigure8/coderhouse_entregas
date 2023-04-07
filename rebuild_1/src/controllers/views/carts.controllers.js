5ae67fdc04965d7db1eb12c7271a39fc4ee70454
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-04-07 19:41:32 -0300

    Add post cart

--- rebuild_1/src/controllers/views/carts.controllers.js
+++ rebuild_1/src/controllers/views/carts.controllers.js
@@ -6,3 +6,12 @@ export const getCart = async(req, res) => {
         res.redirect('/products')
     }
 }
+
+export const postCart = async(req, res) => {
+    const user = req.user;
+    if(user){
+        res.render('carts');
+    } else {
+        res.redirect('/products')
+    }
+}
