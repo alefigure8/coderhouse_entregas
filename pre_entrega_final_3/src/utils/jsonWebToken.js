84cef6c3f8f58d6293951dcc54f9c13acb9a9778
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-04-19 18:00:14 -0300

    Add try catch

--- pre_entrega_final_3/src/utils/jsonWebToken.js
+++ pre_entrega_final_3/src/utils/jsonWebToken.js
@@ -2,7 +2,11 @@ import jwt from "jsonwebtoken";
 import { config } from "./config.js";
 
 export const generateToken = async (user) => {
-  // eslint-disable-next-line no-undef
-  const token = jwt.sign(user, config.jwtSecret, { expiresIn: "1h" });
+  try {
+    // eslint-disable-next-line no-undef
+  const token =  jwt.sign(user, config.jwtSecret, { expiresIn: "1h" });
   return token;
+  } catch (error) {
+    console.log(error);
+  }
 };
