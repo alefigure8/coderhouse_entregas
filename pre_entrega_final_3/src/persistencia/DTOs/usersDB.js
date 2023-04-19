7171f3ddf766637b7b8291173167079f0b6adb91
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-04-19 18:00:21 -0300

    update

--- pre_entrega_final_3/src/persistencia/DTOs/usersDB.js
+++
@@ -1,9 +0,0 @@
-export default class UserssResponseDTO {
-  constructor(user) {
-    this.id = user?.id || user?._id;
-    this.firstname = user.firstname;
-    this.lastname = user.lastname;
-    this.email = user.email;
-    this.password = user.password;
-  }
-}
--- pre_entrega_final_3/src/persistencia/DTOs/usersResponse.js
+++
@@ -1,9 +0,0 @@
-export default class UserssResponseDTO {
-    constructor(user) {
-      this.firstname = user.firstname;
-      this.lastname = user.lastname;
-      this.email = user.email;
-      this.password = user.password;
-    }
-  }
-  
