9e3a921b3d176a93fc5f711f8043553e8240f44a
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-04-22 19:02:49 -0300

    Fix titulos a upper

--- pre_entrega_final_3/src/persistencia/DTOs/productsResponse.dto.js
+++ pre_entrega_final_3/src/persistencia/DTOs/productsResponse.dto.js
@@ -2,7 +2,7 @@ export default class ProductsResponseDTO {
   constructor(product) {
     this.id = product?.id || product?._id;
     this.code = product.code;
-    this.title = product.title;
+    this.title = product.title.toUpperCase();
     this.description = product.description;
     this.thumbnail = product.thumbnail;
     this.price = product.price;
